1e58131231bd977ad371fccb40c78f741ec80bbf
Patrice <kigoto4@gmail.com>
2019-06-30 12:22:50 -0400

    adding and configuring the site

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 // alert("change the text");
-// var name = "jason";
+// var name = "Tomika";
 //
 // var name2 = "name";
 //
 // alert("hello " + name + " " + name2)
+
+// alert("hello stay away")
+// prompt("If you like this page type yes")
+// confirm("Confirm if you are 18")
+var name = "Everyone";
+alert( "Hello "+ name + "!" + ' Welcome to My First WebPage');
+
+// var age = 19
+// confirm("are you 18 +?")
+// if(confirm){
+//   alert("Come in!")
+// }
+// function showAlert(){
+//   alert("Welcome");
+// }
+showAlert();
