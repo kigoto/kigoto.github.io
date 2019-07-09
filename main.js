eeb98b158927ae6af33e57987e70aad0f8020f30
Patrice <kigoto4@gmail.com>
2019-07-09 14:28:08 -0400

    polishing

--- main.js
+++ main.js
@@ -8,15 +8,16 @@
 // alert("hello stay away")
 // prompt("If you like this page type yes")
 // confirm("Confirm if you are 18")
-var name = "Everyone";
-alert( "Hello "+ name + "!" + ' Welcome to My First WebPage');
+// prompt("Hope you like my WebPage")
+//var name = "Everyone";
+//alert( "Hello "+ name + "!" + ' Welcome to My First WebPage');
 
 // var age = 19
-// confirm("are you 18 +?")
+//confirm("Hope you will like it?")
 // if(confirm){
 //   alert("Come in!")
 // }
 // function showAlert(){
-//   alert("Welcome");
+  alert("Welcome");
 // }
 // showAlert()
