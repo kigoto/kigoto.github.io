abb7bb70f6aa97b95cea930c492fd0c8f86ec61f
Patrice <kigoto4@gmail.com>
2019-07-20 09:25:08 -0400

    touch up

--- main.js
+++ main.js
@@ -9,15 +9,15 @@
 // prompt("If you like this page type yes")
 // confirm("Confirm if you are 18")
 // prompt("Hope you like my WebPage")
-//var name = "Everyone";
-//alert( "Hello "+ name + "!" + ' Welcome to My First WebPage');
-
+// var name = "Everyone";
+// alert( "Hello "+ name + "!" + ' Welcome to My First WebPage',
 // var age = 19
 //confirm("Hope you will like it?")
 // if(confirm){
-//   alert("Come in!")
+  alert("Come in!")
 // }
 // function showAlert(){
-  // alert("Welcome");
+//   alert("Welcome );
+  // During this presentation I may get Tongue Tied, please Bear with me");
 // }
-// showAlert()
+// showAlert(Disclamer!!)
